3e42911f8293be0afdb7fdfa9221fad9041634db
ak232 <ajay.ashubaba@gmail.com>
2018-10-27 22:27:51 +0530

    Create userp.vue

---
+++ userp.vue
@@ -0,0 +1,6 @@
+<q-card-separator />
+
+<div class='q-my-xs q-ml-sm text-weight-medium col fnt' >
+	
+<a class='fnt'><span style='color:grey'>Following</span> 299</a>&bull; <a class='fnt'><span style='color:grey'>Followers</span> 5</a></div>
+  <q-card-separator />
